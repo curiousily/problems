7dc7447819743ee8e9907b3127c63c58077b2f50
Venelin Valkov <v.valkov@ymail.com>
2011-08-22 22:58:17 +0300

    added 2:2.1:the.castle

---
+++ usaco/chapter-2/section-2.1/the castle/main.cpp
@@ -0,0 +1,25 @@
+/*
+ID: v.valko1
+PROG: castle
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    freopen("castle.in", "r", stdin);
+    freopen("castle.out", "w", stdout);
+    return 0;
+}
