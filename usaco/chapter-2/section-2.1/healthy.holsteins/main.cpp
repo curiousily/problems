1e112ee07f4c9a8d8ad90d7ec253969d83ccb974
Venelin Valkov <v.valkov@ymail.com>
2011-08-26 23:12:13 +0300

    Added Healthy Holsteins initial template

---
+++ usaco/chapter-2/section-2.1/healthy.holsteins/main.cpp
@@ -0,0 +1,25 @@
+/*
+ID: v.valko1
+PROG: holstein
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    freopen("holstein.in", "r", stdin);
+//    freopen("holstein.out", "w", stdout);
+    return 0;
+}
