2aa72e7b8b0413058ada3c1a5ad3c9010e5abeb5
Venelin Valkov <v.valkov@ymail.com>
2011-08-28 20:34:02 +0300

    Initial Hamming Codes template

---
+++ usaco/chapter-2/section-2.1/hamming.codes/main.cpp
@@ -0,0 +1,26 @@
+/*
+ID: v.valko1
+PROG: hamming
+LANG: C++
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    freopen("hamming.in", "r", stdin);
+//    freopen("hamming.out", "w", stdout);
+
+    return 0;
+}
