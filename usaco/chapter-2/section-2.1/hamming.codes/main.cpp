40e13e5aee7fdcf03882a99736733a961eaab965
Venelin Valkov <venelin@masconsult.eu>
2011-08-29 17:23:36 +0300

    Initial solving...

--- usaco/chapter-2/section-2.1/hamming.codes/main.cpp
+++ usaco/chapter-2/section-2.1/hamming.codes/main.cpp
@@ -21,18 +21,80 @@ using namespace std;
 
 int n, b, d;
 
-vector<string> codewords;
+vector<int> codeWords;
+
+void PrintInBinary(int num) {
+    printf("%s\n", bitset<8>(num).to_string().c_str());
+}
+
+void PrintOutput() {
+    PrintInBinary(0);
+    PrintInBinary(7);
+    PrintInBinary(25);
+    PrintInBinary(30);
+    PrintInBinary(42);
+    PrintInBinary(45);
+    PrintInBinary(51);
+    PrintInBinary(52);
+    PrintInBinary(75);
+    PrintInBinary(76);
+    PrintInBinary(82);
+    PrintInBinary(85);
+    PrintInBinary(97);
+    PrintInBinary(102);
+    PrintInBinary(120);
+    PrintInBinary(127);
+}
+
+void InitSeed() {
+    codeWords.push_back(0);
+    int firstSeed = pow(2, d) - 1;
+    codeWords.push_back(firstSeed);
+    firstSeed++;
+    int secondSeed = firstSeed << 1;
+    secondSeed += firstSeed + 1;
+    codeWords.push_back(secondSeed);
+}
+
+void Generate() {
+//    for(unsigned i = 3; codeWords.size() < n; i++) {
+//        codeWords.push_back(codeWords[1] - codeWords[i - 1]);
+//    }
+}
 
 int main()
 {
+
+    //// vsichki stoinosti koito 2 po 2 davat seed-a ?
     freopen("hamming.in", "r", stdin);
 //    freopen("hamming.out", "w", stdout);
 
     scanf("%d %d %d\n", &n, &b, &d);
+//
+//    InitSeed();
+//    Generate();
+//
+//    for(unsigned i = 0; i < n; i++) {
+//        printf("%d\n", codeWords[i]);
+//    }
 
-    int a = 0;
-    string rep = bitset<8>(a).to_string();
-    cout << rep << endl;
+    unsigned char a = 52;
+    unsigned char b = 51;
+    unsigned char c;
 
+    c = a ^ b;
+//    cout << "before" << endl;
+    cout << (int) c << endl;
+//    cout << "after" << endl;
+
+//    int x = pow(2, 3);
+//    int shifter = 1;
+//    int res = x;
+//    x = x << shifter;
+//    res += x;
+//    res += 1;
+//    PrintInBinary(res);
+
+//    PrintOutput();
     return 0;
 }
