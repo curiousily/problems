bf53e97e3d1ce7845d8a556991ca9f2c64f8b64e
Venelin Valkov <venelin@naughtyspirit.com>
2012-02-13 21:55:46 +0200

    Added new library of generic algorithms

---
+++ algorithms/graphs/prim_mst.cpp
@@ -0,0 +1,98 @@
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <cstdio>
+#include <climits>
+#include <ctype.h>
+#include <algorithm>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+#define FOR(upperLimit) \
+  for(unsigned i = 0; i < upperLimit; i++)
+
+#define FOREACH(var, upperLimit) \
+  for(unsigned var = 0; var < upperLimit; var++)
+
+#define MAX_NODES 401
+#define MAX_WEIGHT INT_MAX
+
+unsigned Nodes;
+
+unsigned AdjMatrix[MAX_NODES][MAX_NODES];
+bool InTree[MAX_NODES];
+
+unsigned StartNode;
+
+const int RESULT_IMPOSSIBLE = -1;
+
+int
+FindDistance()
+{
+  FOREACH(i, Nodes)
+  {
+    InTree[i] = false;
+  }
+  unsigned minWeightFound, edgeStartNode, edgeEndNode;
+  unsigned edgesTraversed = 0, totalDistance = 0;
+  InTree[StartNode] = true;
+  while (edgesTraversed < Nodes - 1)
+  {
+    minWeightFound = MAX_WEIGHT;
+    FOREACH(i, Nodes)
+    {
+      if (InTree[i])
+      {
+        FOREACH(j, Nodes)
+        {
+          if (!InTree[j])
+          {
+            if (minWeightFound > AdjMatrix[i][j])
+            {
+              minWeightFound = AdjMatrix[i][j];
+              edgeStartNode = i;
+              edgeEndNode = j;
+            }
+          }
+        }
+      }
+    }
+    // No edge found
+    if (minWeightFound == MAX_WEIGHT)
+    {
+      return RESULT_IMPOSSIBLE;
+    }
+    totalDistance += AdjMatrix[edgeStartNode][edgeEndNode];
+    InTree[edgeEndNode] = true;
+    edgesTraversed++;
+  }
+  return totalDistance;
+}
+
+int
+main()
+{
+  FOREACH(i, Nodes)
+  {
+    FOREACH(j, Nodes)
+    {
+      AdjMatrix[i][j] = MAX_WEIGHT;
+    }
+  }
+  // Fill the matrix here
+  int result = FindDistance();
+  if (result == RESULT_IMPOSSIBLE)
+  {
+    // invalid result
+  }
+  else
+  {
+    // valid result
+  }
+  return 0;
+}
