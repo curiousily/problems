a14710ef51d808099765170b65a0440e1d2f3ffc
Venelin Valkov <venelin@bapplz.com>
2012-08-13 19:51:57 +0300

    AC 913 - Joana and the Odd Numbers

---
+++ uva/913/src/913.cpp
@@ -0,0 +1,48 @@
+#include <fcntl.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <ctype.h>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#define FOR(upperLimit) \
+  for(unsigned i = 0; i < upperLimit; i++)
+
+#define FOREACH(var, upperLimit) \
+  for(unsigned var = 0; var < upperLimit; var++)
+
+#define READINT ({int num;scanf("%d",&num);num;});
+#define READLONG ({long num;scanf("%ld",&num);num;});
+#define READSTRING ({string strLine; cin >> strLine; strLine;});
+#define READLINE ({string strLine; getline(cin, strLine); strLine;});
+
+long long N;
+
+int
+main()
+{
+#ifndef ONLINE_JUDGE
+  close(0);
+  open("in.txt", O_RDONLY);
+//          close (1); open ("out.txt", O_WRONLY | O_CREAT, 0600);
+#endif
+
+  while (scanf("%lld\n", &N) != EOF)
+  {
+    long long lastNumber = ((N * (N + 2)) / 2);
+    long long sum = lastNumber * 3 - 6;
+    printf("%lld\n", sum);
+  }
+
+  return 0;
+}
