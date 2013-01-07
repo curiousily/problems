01aac6258df79934f63ad4082f6f11e992646023
Venelin Valkov <venelin@bapplz.com>
2013-01-07 19:05:56 +0200

    Added Fraction To Decimals initial project

---
+++ usaco/chapter-2/section-2.4/fracdec/src/fracdec.cpp
@@ -0,0 +1,41 @@
+/*
+ ID: v.valko1
+ PROG: fracdec
+ LANG: C++
+ */
+
+#include <fcntl.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <ctype.h>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#define FOR(upperLimit) \
+  for(unsigned i = 0; i < upperLimit; i++)
+
+#define FOREACH(var, upperLimit) \
+  for(unsigned var = 0; var < upperLimit; var++)
+
+#define READINT ({int num;scanf("%d",&num);num;});
+#define READLONG ({long num;scanf("%ld",&num);num;});
+#define READSTRING ({string strLine; cin >> strLine; strLine;});
+#define READLINE ({string strLine; getline(cin, strLine); strLine;});
+
+int main() {
+
+	freopen("fracdec.in", "r", stdin);
+//	freopen("fracdec.out", "w", stdout);
+
+	return 0;
+}
