abdbcc4b65e36c74d56cd97581d88a8bcff496b2
Venelin Valkov <venelin@bapplz.com>
2013-01-14 19:06:52 +0200

    AC Fractions to Decimals

--- usaco/chapter-2/section-2.4/fracdec/src/fracdec.cpp
+++ usaco/chapter-2/section-2.4/fracdec/src/fracdec.cpp
@@ -32,10 +32,79 @@ using namespace std;
 #define READSTRING ({string strLine; cin >> strLine; strLine;});
 #define READLINE ({string strLine; getline(cin, strLine); strLine;});
 
+const unsigned int MAX_LENGHT_PER_LINE = 76;
+
+int N, D;
+
+struct Remainder {
+	bool used;
+	int position;
+};
+
+string Answer;
+Remainder Remainders[100000];
+
+string IntToString(int i) {
+	ostringstream outstr;
+	outstr << i;
+	return outstr.str();
+}
+
 int main() {
 
 	freopen("fracdec.in", "r", stdin);
-//	freopen("fracdec.out", "w", stdout);
+	freopen("fracdec.out", "w", stdout);
+
+	for (int i = 0; i < 100000; i++) {
+		Remainders[i].used = false;
+		Remainders[i].position = 0;
+	}
+
+	N = READINT;
+	D = READINT;
+
+	if (N >= D) {
+		int wholePart = N / D;
+		N = N % D;
+		Answer = IntToString(wholePart) + ".";
+	} else {
+		Answer = "0.";
+	}
+
+	if (N == 0) {
+		printf("%s0\n", Answer.c_str());
+		return 0;
+	}
+
+	while (true) {
+		if (N == 0) {
+			printf("%s\n", Answer.c_str());
+			return 0;
+		}
+
+		if (Remainders[N].used == true) {
+			Answer.insert(Remainders[N].position, 1, '(');
+			Answer += ')';
+
+			break;
+		} else {
+			Remainders[N].used = true;
+			Remainders[N].position = Answer.length();
+
+			N *= 10;
+
+			int wholePart = N / D;
+			N = N % D;
+			Answer += IntToString(wholePart);
+		}
+	}
+
+	while (Answer.length() > MAX_LENGHT_PER_LINE) {
+		printf("%s\n", Answer.substr(0, MAX_LENGHT_PER_LINE).c_str());
+		Answer = Answer.substr(MAX_LENGHT_PER_LINE);
+	}
+
+	printf("%s\n", Answer.c_str());
 
 	return 0;
 }
